1c37636b06758d90aee393cdc1f2b8e8366519a0
Benoit Landry <ben.acc.landry@gmail.com>
2018-11-07 10:50:23 -0800

    moving toy problem to self contained notebook

--- src/BilevelTrajOpt.jl
+++ src/BilevelTrajOpt.jl
@@ -1,24 +1,11 @@
 module BilevelTrajOpt
 
-export contact_constraints,
-        contact_constraints_implicit,
-        simulate,
-        simulate_implicit,
-        contact_forces,
-        dcontact_forces,
-        newton_contact_forces,
-        dnewton_contact_forces
+export 
 
 using StaticArrays
 using ForwardDiff
 using Ipopt
-using Rotations
-using RigidBodyDynamics
-using RigidBodyDynamics.Contact
-using RigidBodyTreeInspector
-using DrakeVisualizer
 using OSQP
-using Parametron
 
 include("contact.jl")
 include("simulate.jl")
